18ea2f0f9ed27113b7f6532ffacb6a290f3edfcd
AliBoraik <ali99aqel@gmail.com>
2024-11-11 21:40:53 +0300

    add descriptions for the task Container With Most Water

--- LeetCode/Tasks/LeetCode_75/ContainerWithMostWater.cs
+++ LeetCode/Tasks/LeetCode_75/ContainerWithMostWater.cs
@@ -2,6 +2,15 @@ namespace LeetCode.Tasks.LeetCode_75;
 
 public class ContainerWithMostWater
 {
+    /*
+     * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+       
+       Find two lines that together with the x-axis form a container, such that the container contains the most water.
+       
+       Return the maximum amount of water a container can store.
+       
+       Notice that you may not slant the container.
+     */
     public int MaxArea(int[] height) {
         int l=0;
         int r=height.Length-1;
