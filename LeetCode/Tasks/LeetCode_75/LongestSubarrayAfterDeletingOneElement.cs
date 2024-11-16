504c55ef1d37bb3b251f29b7280089a64892c6b5
AliBoraik <ali99aqel@gmail.com>
2024-11-16 19:49:58 +0300

    add task #1493. Longest Subarray of 1's After Deleting One Element

---
+++ LeetCode/Tasks/LeetCode_75/LongestSubarrayAfterDeletingOneElement.cs
@@ -0,0 +1,33 @@
+namespace LeetCode.Tasks.LeetCode_75;
+
+public class LongestSubarrayAfterDeletingOneElement
+{
+    /*
+     Given a binary array nums, you should delete one element from it.
+       
+       Return the size of the longest non-empty subarray containing only 
+       1's in the resulting array. Return 0 if there is no such subarray.
+     */
+    public int LongestSubarray(int[] nums) {
+        int prevWindow = 0;
+        int currWindow = 0;
+        int maxLength = 0;
+        
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == 0) {
+                maxLength = Math.Max(maxLength, prevWindow + currWindow);
+                prevWindow = currWindow;
+                currWindow = 0;
+            } else {
+                currWindow++;
+            }
+        }
+
+        if (currWindow == nums.Length) {
+            return currWindow - 1;
+        }
+
+        maxLength = Math.Max(maxLength, prevWindow + currWindow);
+        return maxLength;
+    }
+}
