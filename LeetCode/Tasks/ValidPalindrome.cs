4a5dd86309b2cd8c18e3e399d3f4c276609d4967
AliBoraik <ali99aqel@gmail.com>
2024-11-22 22:54:46 +0300

    add task #Valid Palindrome

---
+++ LeetCode/Tasks/ValidPalindrome.cs
@@ -0,0 +1,28 @@
+namespace LeetCode.Tasks;
+
+public class ValidPalindrome
+{
+    public bool IsPalindrome(string s) 
+    {
+        for (int i = 0, j = s.Length - 1 ; j > i ; )
+        {
+            if ( !char.IsLetterOrDigit(s[i]) )
+            {
+                i++;
+                continue;
+            }
+
+            if ( !char.IsLetterOrDigit(s[j]) )
+            {
+                j--;
+                continue;
+            }
+
+            if ( char.ToLower(s[i++]) != char.ToLower(s[j--]) )
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
